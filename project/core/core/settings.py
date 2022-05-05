89eb013a4eaac49cc72d5fa9aa84f2aef452eaec
RatChef24 <61520192+RatChef24@users.noreply.github.com>
2022-05-05 14:06:20 +0200

    port add subject

--- project/core/core/settings.py
+++ project/core/core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +146,5 @@ MESSAGE_TAGS = {
         messages.WARNING: 'alert-warning',  #yellow
         messages.ERROR: 'alert-danger',     #red
 }
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
