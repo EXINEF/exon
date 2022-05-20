1fb5d8f3695b43bc39e75921bffe04cc0b5b3c21
EXINEF <alessio.pierdominici@studenti.unicam.it>
2022-05-20 12:11:05 +0200

    separeted js from exam page

---
+++ project/core/static/js/exam.js
@@ -0,0 +1,29 @@
+
+
+function makeTimer(expiration_time) {
+    var endTime=new Date(expiration_time * 1000);
+     endTime = (Date.parse(endTime) / 1000);
+ 
+         var now = new Date();
+         now = (Date.parse(now) / 1000);
+ 
+         var timeLeft = endTime - now;
+ 
+         var hours = Math.floor(timeLeft / 3600);
+         var minutes = Math.floor((timeLeft - (hours * 3600 )) / 60);
+         var seconds = Math.floor((timeLeft - (hours * 3600) - (minutes * 60)));
+
+         if (hours==0 && minutes==0 && seconds==0){
+             alert('TIME EXPIRED')
+             window.location.reload();
+         }
+
+         if (hours < "10") { hours = "0" + hours; }
+         if (minutes < "10") { minutes = "0" + minutes; }
+         if (seconds < "10") { seconds = "0" + seconds; }
+
+         $("#hours").html(hours);
+         $("#minutes").html(minutes);
+         $("#seconds").html(seconds);
+ 
+ }
