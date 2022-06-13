8ee5ee570b2148870022ce0ab628798c50df0991
EXINEF <alessio.pierdominici@studenti.unicam.it>
2022-06-13 19:20:36 +0200

    Fix email problem

--- core/settings.py
+++ core/settings.py
@@ -161,7 +161,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'testemailunicamproject@gmail.com'
-EMAIL_HOST_PASSWORD = 'Project2021'
+EMAIL_HOST_PASSWORD = 'malzgnqeptvynxbs'
 
 
 # DEFINE VARIABLE CONFIGURATION
